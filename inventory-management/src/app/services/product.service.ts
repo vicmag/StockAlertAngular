77d469b040c0ad3dcffc17cd95e2f3648aa7dfd8
vmarquez <vmarquez@minsait.com>
2025-04-24 22:40:35 +0000

    Fase Refactor

--- inventory-management/src/app/services/product.service.ts
+++ inventory-management/src/app/services/product.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../models/product.model';
 import { ProductRepository } from '../repositories/product.repository';
 
 @Injectable({
@@ -8,8 +9,20 @@ export class ProductService {
   constructor(private repository: ProductRepository) {}
 
   increaseStock(productName: string, increment: number): void {
-    const product = this.repository.findByName(productName);
-    product!.stock += increment;
-    this.repository.save(product!);
+    const product = this.findProduct(productName);
+    this.incrementStock(product, increment);
+    this.persistProduct(product);
+  }
+
+  private findProduct(name: string): Product {
+    return this.repository.findByName(name)!;
+  }
+
+  private incrementStock(product: Product, amount: number): void {
+    product.stock += amount;
+  }
+
+  private persistProduct(product: Product): void {
+    this.repository.save(product);
   }
 }
